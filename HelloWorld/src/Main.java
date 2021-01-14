4326d7c751c0919e27c404cb6d8c2719e5998a30
JMB <jbruel@gmail.com>
2021-01-14 15:41:46 +0100

    :robot: Deploy updates

---
+++ HelloWorld/src/Main.java
@@ -0,0 +1,10 @@
+package org.jmb;
+
+
+public class Main {
+	public static void main(String[] args) {
+	      org.jmb.HelloWorld h = new org.jmb.HelloWorld();
+	      h.setName("JMB");
+	      System.out.println(h.getMessage());
+	}
+}
